9e5e3da8e38f8437c55a0270ac770293b700bcde
mayuresh-25 <ddmayuresh@gmail.com>
2025-11-24 19:12:32 +0400

    Create exp1.c

---
+++ exp1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n Name: Mayuresh Devrukhkar");
+    printf("\n Age: 17");
+    printf("\n Class: FY Computer Engineering");
+    printf("\n Div: C");
+    printf("\n UIN: 251P071");
+
+    return 0;
+}
